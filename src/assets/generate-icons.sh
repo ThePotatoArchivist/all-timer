990ba7b99a90baa2529e34b9c66b6f42f06e5d3c
ThePotatoArchivist <josiahfu@gmail.com>
2025-04-02 12:10:27 -0700

    Fix

--- src/assets/generate-icons.sh
+++ src/assets/generate-icons.sh
@@ -6,7 +6,6 @@ declare -A sizes=(
     ["icons/apple-touch-icon.png"]=180
     ["icons/favicon-16x16.png"]=16
     ["icons/favicon-32x32.png"]=32
-    ["icons/favicon-48x48.png"]=32
     ["icons/favicon-64x64.png"]=64
     ["icons/mstile-150x150.png"]=150
 )
@@ -17,4 +16,4 @@ for i in "${!sizes[@]}"; do
 done
 
 echo "Generating favicon.ico"
-magick -background transparent icons/favicon-48x48.png ../../public/favicon.ico
+magick -background transparent icons/favicon-32x32.png ../../public/favicon.ico
